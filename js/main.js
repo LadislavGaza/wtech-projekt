5c90b6173f338f62c5f1e8e347c8fc65216aab95
LadislavGaza <ladislavgaza1@gmail.com>
2021-11-06 01:53:13 +0100

    Refactor, logical model

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ window.addEventListener('load', function() {
     const buy = document.getElementById('add-to-cart');
 
     buy.addEventListener('click', function() {
-        const icon = document.getElementById('cart-add-icon');
         const notif = document.getElementById('cart-notification');
         
         this.innerHTML = '<img class="icon" id="cart-add-icon" src="icons/check.svg">Pridané do košíka';
         this.style.color = 'white';
         this.style.backgroundColor = 'green';
+        
         notif.style.display = 'grid';
     });    
 })
